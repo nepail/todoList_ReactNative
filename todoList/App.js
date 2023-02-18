cd3660157b9d056f5e463b8dad07dae4a1548814
Nepail <kls18z@hotmail.com>
2023-02-18 17:10:32 +0800

    import Stack.Navigator

--- todoList/App.js
+++ todoList/App.js
@@ -10,103 +10,132 @@ import {
 } from "react-native";
 import Task from "./components/Task.js";
 
-export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import Home from './screens/Home';
+import Details from './screens/Details';
 
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task]);
-    setTask(null);
-  };
+const Stack = createStackNavigator();
 
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
-  };
-
-  return (
-    <View style={styles.container}>
-      {/* Today's Task */}
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's task</Text>
-
-        <View style={styles.items}>
-          {/* This is where the tasks will go */}
-          {taskItems.map((item, index) => {
-            return (
-              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                <Task text={item} />
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-      </View>
-
-      {/* Write a task */}
-      <KeyboardAvoidingView
-        befavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder={"Write a task"}
-          value={task}
-          onChangeText={(text) => setTask(text)}
-        />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-    </View>
-  );
+const theme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E8EAED",
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: "absolute",
-    bottom: 60,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignContent: "center",
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: "#FFF",
-    borderRadius: 60,
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: "#FFF",
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-  },
-  addText: {},
-});
+const App = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouterName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+// export default function App() {
+//   const [task, setTask] = useState();
+//   const [taskItems, setTaskItems] = useState([]);
+
+//   const handleAddTask = () => {
+//     Keyboard.dismiss();
+//     setTaskItems([...taskItems, task]);
+//     setTask(null);
+//   };
+
+//   const completeTask = (index) => {
+//     let itemsCopy = [...taskItems];
+//     itemsCopy.splice(index, 1);
+//     setTaskItems(itemsCopy);
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       {/* Today's Task */}
+//       <View style={styles.tasksWrapper}>
+//         <Text style={styles.sectionTitle}>Today's task</Text>
+
+//         <View style={styles.items}>
+//           {/* This is where the tasks will go */}
+//           {taskItems.map((item, index) => {
+//             return (
+//               <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+//                 <Task text={item} />
+//               </TouchableOpacity>
+//             );
+//           })}
+//         </View>
+//       </View>
+
+//       {/* Write a task */}
+//       <KeyboardAvoidingView
+//         befavior={Platform.OS === "ios" ? "padding" : "height"}
+//         style={styles.writeTaskWrapper}
+//       >
+//         <TextInput
+//           style={styles.input}
+//           placeholder={"Write a task"}
+//           value={task}
+//           onChangeText={(text) => setTask(text)}
+//         />
+//         <TouchableOpacity onPress={() => handleAddTask()}>
+//           <View style={styles.addWrapper}>
+//             <Text style={styles.addText}>+</Text>
+//           </View>
+//         </TouchableOpacity>
+//       </KeyboardAvoidingView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#E8EAED",
+//   },
+//   tasksWrapper: {
+//     paddingTop: 80,
+//     paddingHorizontal: 20,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//   },
+//   items: {
+//     marginTop: 30,
+//   },
+//   writeTaskWrapper: {
+//     position: "absolute",
+//     bottom: 60,
+//     width: "100%",
+//     flexDirection: "row",
+//     justifyContent: "space-around",
+//     alignContent: "center",
+//   },
+//   input: {
+//     paddingVertical: 15,
+//     paddingHorizontal: 15,
+//     backgroundColor: "#FFF",
+//     borderRadius: 60,
+//     borderColor: "#C0C0C0",
+//     borderWidth: 1,
+//     width: 250,
+//   },
+//   addWrapper: {
+//     width: 60,
+//     height: 60,
+//     backgroundColor: "#FFF",
+//     borderRadius: 60,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     borderColor: "#C0C0C0",
+//     borderWidth: 1,
+//   },
+//   addText: {},
+// });
