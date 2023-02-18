4ebad92e4f89c260cf4a3f33fcfefb241cf4a78f
Nepail <kls18z@hotmail.com>
2023-02-18 15:51:56 +0800

    add Task components

--- todoList/App.js
+++ todoList/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      {/* Today's Task */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's task</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+          <Task text={'Task 3'}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30
   },
 });
