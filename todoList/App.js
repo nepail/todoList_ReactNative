bac3b6113d5bcb8d05633b162989dc70c44d91e2
Nepail <kls18z@hotmail.com>
2023-02-18 17:27:28 +0800

    add assets

--- todoList/App.js
+++ todoList/App.js
@@ -13,23 +13,36 @@ import Task from "./components/Task.js";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import Home from './screens/Home';
-import Details from './screens/Details';
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
 const Stack = createStackNavigator();
 
 const theme = {
   ...DefaultTheme,
-  colors:{
+  colors: {
     ...DefaultTheme.colors,
-    background: "transparent"
-  }
-}
+    background: "transparent",
+  },
+};
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if(!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouterName="Home">
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouterName="Home"
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
