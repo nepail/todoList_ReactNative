26ed227e501ea1b05ec702be014209f8f76a8a93
Nepail <kls18z@hotmail.com>
2023-02-18 16:07:31 +0800

    add writeTaskWrapper

--- todoList/App.js
+++ todoList/App.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from "react-native";
 import Task from "./components/Task.js";
 
 export default function App() {
@@ -10,11 +17,24 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go */}
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'}/>
-          <Task text={'Task 3'}/>
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+          <Task text={"Task 3"} />
         </View>
       </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        befavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Write a task"} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -33,6 +53,34 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   items: {
-    marginTop: 30
+    marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: "space-around",
+    alignContent: "center"
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addText: {},
 });
