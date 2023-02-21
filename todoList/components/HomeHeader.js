1209d5c256826854ca0ef544b2ad58497712ebad
Nepail <kls18z@hotmail.com>
2023-02-21 10:32:11 +0800

    add Header

--- todoList/components/HomeHeader.js
+++ todoList/components/HomeHeader.js
@@ -1,12 +1,97 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from "react-native";
+import { COLORS, FONTS, SIZES, assets } from "../constants";
 
 const HomeHeader = () => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
-    </View>
-  )
-}
+    <View
+      style={{
+        padding: SIZES.font,
+        backgroundColor: COLORS.primary,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMethod="contain"
+          style={{ width: 90, height: 25 }}
+        />
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 15,
+              height: 15,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+        </View>
+      </View>
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}
+        >
+          Hello, Nepail 🐍🐍🐍
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.white,
+            marginTop: SIZES.base / 2,
+          }}
+        >
+          Let's find a masterpice
+        </Text>
+      </View>
 
-export default HomeHeader
+      <View style={{ marginTop: SIZES.font }}>
+        <View
+          style={{
+            width: "100%",
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: "row",
+            alignContent: "center",
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.small - 2,
+          }}
+        >
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{
+              width: 20,
+              height: 20,
+              marginRight: SIZES.base,
+            }}
+          />
+          <TextInput
+            placeholder="Search"
+            style={{ flex: 1 }}
+            onChangeText={() => {}}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HomeHeader;
