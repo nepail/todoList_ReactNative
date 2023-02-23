b4323b928dc82559b4d7c2669d5ee0b0c31811ed
Nepail <kls18z@hotmail.com>
2023-02-23 16:17:05 +0800

    add detail content

--- todoList/screens/Details.js
+++ todoList/screens/Details.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   View,
   Text,
@@ -16,8 +17,20 @@ import {
   DetailsBid,
 } from "../components";
 
+const DetailsHeader = ({data}) => (
+  <View style={{width: '100%', height: 373}}>
+    <Image 
+      source={data.image}
+      resizeMode="cover"
+      style={{width: '100%', height: '100%'}}
+
+    />
+  </View>
+)
+
 const Details = ({route, navigation}) => {
   const {data} = route.params;
+
   return (
     <SafeAreaView style={{flex:1}}>
       <FocusedStatusBar 
@@ -26,10 +39,33 @@ const Details = ({route, navigation}) => {
         translucent={true}
       />
 
-      <View>
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        paddingVertical: SIZES.font,
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        zIndex: 1,
+      }}>
         <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark}/>
-        
       </View>
+
+      <FlatList 
+        data={data.bids}
+        renderItem={({item}) => <DetailsBid bid={item} />}
+        keyExtractor={(item => item.id)}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: SIZES.extraLarge * 3,
+        }}
+        ListHeaderComponent={()=>(
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation}/>
+          </React.Fragment>
+        )}
+      />
     </SafeAreaView>
   )
 };
