24bbcfd8f9ef9a1a091af606a16ff97563cec652
Nepail <kls18z@hotmail.com>
2023-02-24 15:42:36 +0800

    add DetailsBid

--- todoList/components/DetailsBid.js
+++ todoList/components/DetailsBid.js
@@ -1,12 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import { EthPrice } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
 
-const DetailsBid = () => {
+const DetailsBid = ({ bid }) => {
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
-  )
-}
+    <View style={{
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignContent: "center",
+      marginVertical: SIZES.base,
+      paddingHorizontal: SIZES.base
+    }}>
+      <Image
+        source={bid.image}
+        resizeMod="contain"
+        style={{
+          width: 48,
+          height: 48,
+        }}
+      />
 
-export default DetailsBid
+      <View>
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}
+        >
+          Bid placed by {bid.name}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 2,
+            color: COLORS.secondary,
+            marginTop: 3,
+          }}
+        >
+          {bid.date}
+        </Text>
+      </View>
+
+      <EthPrice price={bid.price} />
+      
+    </View>
+  );
+};
+
+export default DetailsBid;
--- todoList/screens/Details.js
+++ todoList/screens/Details.js
@@ -79,6 +79,17 @@ const Details = ({ route, navigation }) => {
             <SubInfo />
             <View style={{ padding: SIZES.font }}>
               <DetailsDesc data={data} />
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bid
+                </Text>
+              )}
             </View>
           </React.Fragment>
         )}
