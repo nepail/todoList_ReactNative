f3d6df8c33b7fff28d3c841ea50d9891e0fa215d
Nepail <kls18z@hotmail.com>
2023-02-24 15:29:30 +0800

    add Details content

--- todoList/components/index.js
+++ todoList/components/index.js
@@ -4,6 +4,8 @@ import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
 
 export {
   HomeHeader,
@@ -13,4 +15,5 @@ export {
   RectButton,
   DetailsDesc,
   DetailsBid,
+  SubInfo
 };
